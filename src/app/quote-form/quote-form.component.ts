baf4737a699b441b2b35094da93a55dd83906d3a
Charles maina <muragemaina09@gmail.com>
2019-04-29 10:19:59 +0300

    created function for form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -3,7 +3,7 @@ import {Quotes} from '../quotes'
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  
 })
 export class QuoteFormComponent implements OnInit {
   newQuotes=new Quotes(0,"","",new Date());
