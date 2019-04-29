700e42599f03c2668ef026e20a409f4fca946cf8
Charles maina <muragemaina09@gmail.com>
2019-04-29 10:12:14 +0300

    initialized

---
+++ src/app/quotes-details/quotes-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
+import { Quotes} from '../quotes'
+@Component({
+  selector: 'app-quotes-details',
+  templateUrl: './quotes-details.component.html',
+  styleUrls: ['./quotes-details.component.css']
+})
+export class QuotesDetailsComponent implements OnInit {
+  @Input() quotes:Quotes;
+    @Output() isComplete= new EventEmitter<boolean>();
+    quoteDelete(complete:boolean){
+      this.isComplete.emit(complete);
+    }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
